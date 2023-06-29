ccc6703c7057bf31ee9bfb2dd04b977fa1f42c83
linhvnde <linh.nh.tyty@gmail.com>
2023-06-29 20:50:49 +0200

    fix: display image of recipe with conditions

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,10 +1,14 @@
 function RecipeCard(props) {
   return (
     <div className="RecipeCard card">
-      <img
-        src="https://static.vecteezy.com/system/resources/previews/008/695/917/original/no-image-available-icon-simple-two-colors-template-for-no-image-or-picture-coming-soon-and-placeholder-illustration-isolated-on-white-background-vector.jpg"
-        alt="no image"
-      />
+      {!props.recipe.image ? (
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/008/695/917/original/no-image-available-icon-simple-two-colors-template-for-no-image-or-picture-coming-soon-and-placeholder-illustration-isolated-on-white-background-vector.jpg"
+          alt="no image"
+        />
+      ) : (
+        <img src={props.recipe.image} alt={props.recipe.name} />
+      )}
       <h3>{props.recipe.name}</h3>
     </div>
   );
