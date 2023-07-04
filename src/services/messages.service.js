72bb1cf0a0db88f6b48448b3a8badd2e1d339b05
linhvnde <linh.nh.tyty@gmail.com>
2023-07-04 21:39:07 +0200

    feat: messagesService to handle messages display

---
+++ src/services/messages.service.js
@@ -0,0 +1,45 @@
+class MessageService {
+  constructor() {
+    this.timeoutId = null;
+    this.defaultTimer = 800; // default duration for message display
+  }
+
+  showMessage = (message, setMessage, timer = this.defaultTimer) => {
+    // Clear the previous timeout if there is one to avoid nested setTimeout
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+
+    setMessage(message);
+
+    // Save the ID of the new timeout so it can be cleared later
+    this.timeoutId = setTimeout(() => {
+      setMessage('');
+    }, timer);
+  };
+
+  showCreateMessage = (itemName, setMessage, timer = this.defaultTimer) => {
+    this.showMessage(`Successfully created ${itemName}`, setMessage, timer);
+  };
+
+  showAddMessage = (itemName, setMessage, timer = this.defaultTimer) => {
+    this.showMessage(`Successfully added ${itemName}`, setMessage, timer);
+  };
+  
+  showWarningMessage = (itemName, setMessage, timer = this.defaultTimer) => {
+    this.showMessage(`Please provide quantity for ${itemName}`, setMessage, timer);
+  };
+
+  showUpdateMessage = (itemName, setMessage, timer = this.defaultTimer) => {
+    this.showMessage(`Successfully updated ${itemName}`, setMessage, timer);
+  };
+
+  showDeleteMessage = (itemName, setMessage, timer = this.defaultTimer) => {
+    this.showMessage(`Successfully deleted ${itemName}`, setMessage, timer);
+  };
+}
+
+// Create one instance object
+const messagesService = new MessageService();
+
+export default messagesService;
