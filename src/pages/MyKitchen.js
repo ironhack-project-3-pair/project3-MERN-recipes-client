fdf853c6d320896f19ff156c03eacb6c2ba5a11d
linhvnde <linh.nh.tyty@gmail.com>
2023-07-04 23:59:08 +0200

    feat: message displays in MyKitchen

--- src/pages/MyKitchen.js
+++ src/pages/MyKitchen.js
@@ -4,12 +4,16 @@ import { Link, useParams } from 'react-router-dom';
 import IngredientCard from '../components/IngredientCard';
 
 import userIngredientsService from '../services/userIngredients.service';
+import messageService from '../services/messages.service';
 
 // import axios from 'axios';
 // const API_URL = 'http://localhost:5005';
 
 function MyKitchen() {
   const [userIngredients, setUserIngredients] = useState([]);
+
+  //messages
+  const [deleteMessage, setDeleteMessage] = useState('');
 
   // // Get the token from the localStorage
   const storedToken = localStorage.getItem('authToken');
@@ -41,13 +45,25 @@ function MyKitchen() {
 
   //delete ingredient from userIngredients
   const deleteUserIngredient = (userIngredientId) => {
+    const userIngredientToDelete = userIngredients.find(
+      (ingredient) => ingredient._id === userIngredientId
+    );
     // axios
     //   .delete(`${API_URL}/api/user-ingredients/${userIngredientId}`, {
     //     headers: { Authorization: `Bearer ${storedToken}` },
     //   })
     userIngredientsService
       .deleteUserIngredient(userIngredientId)
-      .then(() => getUserIngredients())
+      .then((res) => {
+        const deleteUserIngredient = res.data;
+        //show message after deletion
+        messageService.showDeleteMessage(
+          userIngredientToDelete.ingredient.name,
+          setDeleteMessage
+        );
+        //refresh userIngredients
+        getUserIngredients();
+      })
       .catch((error) => {
         console.log('error delete userRecipes: ', error, error.response.data);
       });
@@ -56,7 +72,7 @@ function MyKitchen() {
     <Container>
       <h4 className="row">
         <span className="col-8 text-start">
-          Your Kitchen has: {userIngredients.length===0} Items
+          Your Kitchen - {userIngredients.length} Ingredients
         </span>
         <span className="col-4">
           <Link className="text-decoration-none" to={'/ingredients'}>
@@ -64,6 +80,11 @@ function MyKitchen() {
           </Link>
         </span>
       </h4>
+      <hr />
+
+      {/* Show the message */}
+      {deleteMessage && <p>{deleteMessage}</p>}
+
       {userIngredients.map((ingredient) => {
         return (
           <IngredientCard
